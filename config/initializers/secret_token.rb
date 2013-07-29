8f38d2970623f9305e4c219e9a313c1dbf80ba7a
Nicole Glasgow <nicole.glasgow@ymail.com>
2013-07-29 09:52:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelInsight::Application.config.secret_token = '20f0e12a2719279ec210396138fcd076a9850a577b28deb9895ab46a736440c053265df425c32a750eabba21c9b0f1592164c40fef825a969a6a2eed4d916e79'
